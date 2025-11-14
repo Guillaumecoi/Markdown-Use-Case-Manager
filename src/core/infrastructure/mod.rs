5c1834b969156074c0c037831fbc16698dd208b4
Guillaume <120651399+Guillaumecoi@users.noreply.github.com>
2025-11-14 06:23:21 +0100

    style: Run cargo fmt

--- src/core/infrastructure/mod.rs
+++ src/core/infrastructure/mod.rs
@@ -7,7 +7,9 @@ mod template_engine;
 
 // Re-exports
 pub use languages::LanguageRegistry;
-pub use methodologies::{CustomFieldConfig, Methodology, MethodologyDefinition, MethodologyRegistry};
+pub use methodologies::{
+    CustomFieldConfig, Methodology, MethodologyDefinition, MethodologyRegistry,
+};
 pub use persistence::sqlite::SqliteUseCaseRepository;
 pub use persistence::{
     file_operations, RepositoryFactory, TomlUseCaseRepository, TransactionalRepository,
